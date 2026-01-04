26cd47105cf85fc5c9305b8d2a7a7a50f5b0e834
Yang Zheyue <167635900+yzy2312963@users.noreply.github.com>
2026-01-04 12:19:48 +0800

    Update default_sched.c

--- lab6_fund+challenge1/kern/schedule/default_sched.c
+++ lab6_fund+challenge1/kern/schedule/default_sched.c
@@ -17,7 +17,7 @@
 static void
 RR_init(struct run_queue *rq)
 {
-    // LAB6: YOUR CODE
+    // LAB6: 2312963
     list_init(&(rq->run_list));
     rq->proc_num = 0;
     rq->lab6_run_pool = NULL;
@@ -37,7 +37,7 @@ RR_init(struct run_queue *rq)
 static void
 RR_enqueue(struct run_queue *rq, struct proc_struct *proc)
 {
-    // LAB6: YOUR CODE
+    // LAB6: 2312963
     assert(list_empty(&(proc->run_link)));
     list_add_before(&(rq->run_list), &(proc->run_link));
     if (proc->time_slice == 0 || proc->time_slice > rq->max_time_slice) {
@@ -57,7 +57,7 @@ RR_enqueue(struct run_queue *rq, struct proc_struct *proc)
 static void
 RR_dequeue(struct run_queue *rq, struct proc_struct *proc)
 {
-    // LAB6: YOUR CODE
+    // LAB6: 2312963
     assert(!list_empty(&(proc->run_link)) && proc->rq == rq);
     list_del_init(&(proc->run_link));
     rq->proc_num --;
@@ -74,7 +74,7 @@ RR_dequeue(struct run_queue *rq, struct proc_struct *proc)
 static struct proc_struct *
 RR_pick_next(struct run_queue *rq)
 {
-    // LAB6: YOUR CODE
+    // LAB6: 2312963
     list_entry_t *le = list_next(&(rq->run_list));
     if (le != &(rq->run_list)) {
         return le2proc(le, run_link);
@@ -92,7 +92,7 @@ RR_pick_next(struct run_queue *rq)
 static void
 RR_proc_tick(struct run_queue *rq, struct proc_struct *proc)
 {
-    // LAB6: YOUR CODE
+    // LAB6: 2312963
     if (proc->time_slice > 0) {
         proc->time_slice --;
     }
