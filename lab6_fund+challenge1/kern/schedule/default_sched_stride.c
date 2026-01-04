5f7084d8c685895b8984ba777f20896503ced9ba
Yang Zheyue <167635900+yzy2312963@users.noreply.github.com>
2026-01-04 12:20:58 +0800

    Replace challenge comments with identifier 2312963
    Updated LAB6 CHALLENGE 1 comments with identifier 2312963.

--- lab6_fund+challenge1/kern/schedule/default_sched_stride.c
+++ lab6_fund+challenge1/kern/schedule/default_sched_stride.c
@@ -8,7 +8,7 @@
 #define USE_SKEW_HEAP 1
 
 /* You should define the BigStride constant here*/
-/* LAB6 CHALLENGE 1: YOUR CODE */
+/* LAB6 CHALLENGE 1: 2312963 */
 #define BIG_STRIDE 0x7fffffff /* you should give a value, and is ??? */
 
 /* The compare function for two skew_heap_node_t's and the
@@ -41,7 +41,7 @@ proc_stride_comp_f(void *a, void *b)
 static void
 stride_init(struct run_queue *rq)
 {
-     /* LAB6 CHALLENGE 1: YOUR CODE
+     /* LAB6 CHALLENGE 1: 2312963
       * (1) init the ready process list: rq->run_list
       * (2) init the run pool: rq->lab6_run_pool
       * (3) set number of process: rq->proc_num to 0
@@ -67,7 +67,7 @@ stride_init(struct run_queue *rq)
 static void
 stride_enqueue(struct run_queue *rq, struct proc_struct *proc)
 {
-     /* LAB6 CHALLENGE 1: YOUR CODE
+     /* LAB6 CHALLENGE 1: 2312963
       * (1) insert the proc into rq correctly
       * NOTICE: you can use skew_heap or list. Important functions
       *         skew_heap_insert: insert a entry into skew_heap
@@ -97,7 +97,7 @@ stride_enqueue(struct run_queue *rq, struct proc_struct *proc)
 static void
 stride_dequeue(struct run_queue *rq, struct proc_struct *proc)
 {
-     /* LAB6 CHALLENGE 1: YOUR CODE
+     /* LAB6 CHALLENGE 1: 2312963
       * (1) remove the proc from rq correctly
       * NOTICE: you can use skew_heap or list. Important functions
       *         skew_heap_remove: remove a entry from skew_heap
@@ -123,7 +123,7 @@ stride_dequeue(struct run_queue *rq, struct proc_struct *proc)
 static struct proc_struct *
 stride_pick_next(struct run_queue *rq)
 {
-     /* LAB6 CHALLENGE 1: YOUR CODE
+     /* LAB6 CHALLENGE 1: 2312963
       * (1) get a  proc_struct pointer p  with the minimum value of stride
              (1.1) If using skew_heap, we can use le2proc get the p from rq->lab6_run_pol
              (1.2) If using list, we have to search list to find the p with minimum stride value
@@ -150,7 +150,7 @@ stride_pick_next(struct run_queue *rq)
 static void
 stride_proc_tick(struct run_queue *rq, struct proc_struct *proc)
 {
-     /* LAB6 CHALLENGE 1: YOUR CODE */
+     /* LAB6 CHALLENGE 1: 2312963 */
      if (proc->time_slice > 0)
      {
          proc->time_slice--;
